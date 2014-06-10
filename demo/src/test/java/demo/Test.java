95b9a1d8e93dfa22ed3e819a59b8f71840a4fe4f
wangxiaofei <xiaofei.wang@radida.com>
2014-06-10 16:18:51 +0800

    up

--- demo/src/test/java/demo/Test.java
+++ demo/src/test/java/demo/Test.java
@@ -1,10 +1,10 @@
 package demo;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -19,13 +19,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.shawn.demo.business.utils.DateFormat;
-
 public class Test {
 
-	private static String sid = "831cc356921e4bb9ac1f15aca61f58d7";
+	private static String sid = "e2a0129be2b14a828d8153c88460d81e";
 
-	private static String domain = "http://192.168.1.10:8080/rad";
+	private static String domain = "http://localhost:8080/rad";
 
 	private Logger logger = Logger.getLogger(Test.class);
 
@@ -54,8 +52,112 @@ public class Test {
 		JSONObject obj = new JSONObject();
 		String param = null;
 		try {
-			obj.put("username", "tianbo");
-			obj.put("passwd", "tianbo");
+			obj.put("username", "wangxiaofei");
+			String passwd = "123";
+			passwd=DigestUtils.md5Hex(passwd);
+//			logger.info("password="+passwd);
+			obj.put("passwd", passwd);
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info(param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	public void testReg() {
+		String url = domain + "/user/register";
+		JSONObject obj = new JSONObject();
+		String param = null;
+		try {
+			obj.put("username", "wangxiaofei");
+			obj.put("passwd", "wangxiaofei");
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info(param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	public void testUpdateUser() {
+		String url = domain + "/user/updateuser";
+		JSONObject obj = new JSONObject();
+		String param = null;
+		try {
+			obj.put("sid", sid);
+			obj.put("email", "wangxiaofei340@163.com");
+			obj.put("tel", "18511549100");
+			obj.put("institutionid", "10000");
+			// obj.put("institutionname", "北大第三医院");
+			obj.put("title", "泌尿科专家");
+			obj.put("sex", "1");
+			obj.put("birthday", "1986-04-26");
+			obj.put("age", "45");
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info(param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	public void testGetUser() {
+		String url = domain + "/user/getuser";
+		JSONObject obj = new JSONObject();
+		String param = null;
+		try {
+			obj.put("sid", sid);
+			obj.put("userid", "101277");
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info(param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	public void testGetUser4Friend() {
+		String url = domain + "/user/queryuser";
+		JSONObject obj = new JSONObject();
+		String param = null;
+		try {
+			obj.put("sid", sid);
+			obj.put("name", "单");
+			obj.put("page", "1");
+			obj.put("count", "100");
 			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
 			logger.info(param);
 		} catch (JSONException e) {
@@ -261,6 +363,31 @@ public class Test {
 		}
 	}
 
+	public void testDelReport() {
+		String url = domain + "/diag/delrequest";
+
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("requestid", "103311");
+			obj.put("type", "2");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void testGetReportRequestList() {
 		String url = domain + "/diag/getreportrequestlist";
 
@@ -268,9 +395,68 @@ public class Test {
 		JSONObject obj = new JSONObject();
 		try {
 			obj.put("sid", sid);
-			obj.put("requestid", "103603");
+			// obj.put("requestid", "103603");
 			obj.put("type", "1");
-			obj.put("status", "2");
+			obj.put("status", "1");
+			obj.put("page", "1");
+			obj.put("count", "100");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testSearchReportRequestList() {
+		String url = domain + "/diag/searchreportrequestlist";
+
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			// obj.put("requestid", "103603");
+			// obj.put("patname", "a");
+			obj.put("type", "1");
+			obj.put("status", "1");
+			obj.put("page", "1");
+			obj.put("count", "100");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testQueryReportRequestList() {
+		String url = domain + "/diag/queryreportrequestlist";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			// obj.put("requestid", "103603");
+			obj.put("keywords", "a");
+			// obj.put("patname", "11");
+			obj.put("requestbegintime", "2014-05-05");
+			obj.put("type", "2");
+			obj.put("status", "0");
 			obj.put("page", "1");
 			obj.put("count", "100");
 			logger.info("JSON : " + obj.toString());
@@ -296,9 +482,9 @@ public class Test {
 		JSONObject obj = new JSONObject();
 		try {
 			obj.put("sid", sid);
-			obj.put("requestid", "103604");
+			// obj.put("requestid", "103604");
 			obj.put("type", "1");
-			obj.put("status", "2");
+			obj.put("status", "0,1,2");
 			obj.put("page", "1");
 			obj.put("count", "100");
 
@@ -537,24 +723,429 @@ public class Test {
 		}
 	}
 
+	public void testDelMessage() {
+		String url = domain + "/mess/delmess";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("messid", "107360");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testGetInstitutionsByName() {
+		String url = domain + "/ins/getbyname";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("name", "医");
+			obj.put("page", "1");
+			obj.put("count", "10");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testUploadCallback() {
+		String url = domain + "/cloud/uploadcallback";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("ret", "2");
+			obj.put("entitytype", "1");
+			obj.put("entityid", "103438");
+			JSONArray array = new JSONArray();
+			JSONObject o1 = new JSONObject();
+			o1.put("filename", "filename");
+			o1.put("key", "abfdjfklasdjfjaslfjlas;f");
+			o1.put("domain", "domain.qiniu.com");
+			o1.put("size", "123456789");
+			o1.put("filetype", "1");
+
+			JSONObject o2 = new JSONObject();
+			o2.put("filename", "filename2");
+			o2.put("key", "fasdfadsfdasdfasdfasfd;f");
+			o2.put("domain", "domain.qiniu.com");
+			o2.put("size", "987654321");
+			o2.put("filetype", "2");
+			array.put(o1);
+			array.put(o2);
+			// obj.put("uplist", array);
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testFindPassword() {
+		String url = domain + "/user/findpass";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("username", "wangxiaofei");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testGetDownloadURL() {
+		String url = domain + "/cloud/getdownloadurl";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("id", "105761");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testGetMessageCount() {
+		String url = domain + "/mess/getmesscount";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("status", "0");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testAuth() {
+		String url = domain + "/auth/authaction";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("action", "2");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testCreateRepo() {
+		String url = domain + "/repo/create";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("pid", "1004");
+			obj.put("userid", "-1");
+			obj.put("name", "根节点1-子节点1-子节点1");
+			obj.put("imgdesc", "根节点1-子节点1-子节点1影像描述");
+			obj.put("diagdesc", "根节点1-子节点1-子节点1诊断描述");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testUpdateRepo() {
+		String url = domain + "/repo/update";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("id", "1006");
+			obj.put("pid", "0");
+			obj.put("userid", "-1");
+			obj.put("name", "根节点5");
+			obj.put("imgdesc", "根节点5影像描述");
+			obj.put("diagdesc", "根节点5诊断描述");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testDelRepo() {
+		String url = domain + "/repo/delete";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("id", "1000");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testGetRepo() {
+		String url = domain + "/repo/get";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("pid", "1004");
+			obj.put("status", "-1,0");
+			// obj.put("userid", "");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testAuthUser() {
+		String url = domain + "/user/authuser";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("sid", sid);
+			obj.put("approveid", "101380");
+			obj.put("ispass", "1");
+			obj.put("approvedesc", "Test ApproveDesc");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void testCheckUpdate() {
+		String url = domain + "/app/checkupdate";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			// obj.put("sid", sid);
+			obj.put("version", "1.0.0");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void testBugReport() {
+		String url = domain + "/app/bugreport";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			 obj.put("msg", "abcdefg");
+			obj.put("version", "1.0.0");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void testCreateFeedBack() {
+		String url = domain + "/app/createfeedback";
+		String param = null;
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("content", "abcdefg");
+			obj.put("userid", "1000");
+			obj.put("product", "1");
+			obj.put("email", "aaaaa@aa.com");
+			logger.info("JSON : " + obj.toString());
+			param = new String(Base64.encodeBase64(obj.toString().getBytes()));
+			logger.info("BASE64 : " + param);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		// 构造post参数
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("params", param));
+		try {
+			post(url, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		Test test = new Test();
-		test.testLogin();
+//		 test.testLogin();
 		// test.testCreateReportRequest();
 		// test.testCreateTalkRequest();
 		// test.testCreateTalkRequestReply();
 		// test.testCreateReport();
 		// test.testGetReport();
 		// test.testGetReportRequestList();
+		// test.testSearchReportRequestList();
 		// test.testGetTalkRequestList();
 		// test.testGetRequestAidInfo();
 		// test.testGetRequestInfo();
 		// test.testGetTalkRequestInfo();
-//		test.testGetFriendList();
-//		test.testGetFriendApplyList();
-//		test.testGetFriendAuthList();
-//		test.testAddFriend();
-//		test.testAuthFriend();
-//		test.testDelFriend();
+		// test.testGetFriendList();
+		// test.testGetFriendApplyList();
+		// test.testGetFriendAuthList();
+		// test.testAddFriend();
+		// test.testAuthFriend();
+		// test.testDelFriend();
+		// test.testDelMessage();
+		// test.testGetInstitutionsByName();
+		// test.testReg();
+		// test.testUpdateUser();
+		// test.testGetUser();
+		// test.testGetUser4Friend();
+		// test.testUploadCallback();
+		// test.testFindPassword();
+		// test.testGetDownloadURL();
+		// test.testDelReport();
+		// test.testGetMessageCount();
+		// test.testAuth();
+		// test.testSearchReportRequestList();
+		// test.testQueryReportRequestList();
+		// test.testCreateRepo();
+		// test.testUpdateRepo();
+		// test.testGetRepo();
+		// test.testDelRepo();
+//		test.testCheckUpdate();
+//		 test.testAuthUser();
+//		for(int i =0;i< 1000;i++){
+//			test.testBugReport();
+//		}
+		test.testCreateFeedBack();
+		
 	}
 }
